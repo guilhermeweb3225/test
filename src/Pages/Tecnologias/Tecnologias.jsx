c12b9c898b61637928ad630f06063736abdd44e4
Guilherme P.Oliveira <guilhermeponciano3225@gmail.com>
2024-12-29 15:36:07 -0300

    atualizando os button

--- src/Pages/Tecnologias/Tecnologias.jsx
+++ src/Pages/Tecnologias/Tecnologias.jsx
@@ -1,7 +1,14 @@
 import './Tecnologias.css'
 import Header from '../../componetes/header/Header'
 import Footer from '../../componetes/footer/Footer'
-import Livro from '../../assets/livro.png'
+import Estru from '../../assets/icon.html.png'
+import Style from '../../assets/icon.css.png'
+import Jsc from '/assets/icon-Jsc.png';
+import ReactImg from '/assets/icon-react.png';
+import Ts from '/assets/icon-ts.png';
+import Git from '/assets/icon-git.png';
+import GitIcon from '/assets/git-icon.svg';
+import Figma from '/assets/icon-figma.png';
 
 function Tecnologias(){
     return(
@@ -11,37 +18,37 @@ function Tecnologias(){
         <h1>Tecnologias</h1>
         <section>
             <div className="dv-imgs">
-                <img src={Livro}/>
+                <img src={Estru}/>
                 <div className="texto">
                     <p>Criação de estrutura e conteúdo das páginas web, garantindo acessibilidade e organização.</p>
                 </div>
             </div>
             <div className="dv-imgs">
-                <img src="../../assets/icon-css.png" />
+                <img src={Style}/>
                 <div className="texto">
                     <p> Estilização e layout das páginas, criando designs responsivos e visualmente atraentes.</p>
                 </div>
             </div>
             <div className="dv-imgs">
-                <img src='../../assets/icon-Jsc.png' />
+                <img src={Jsc} />
                 <div className="texto">
                     <p>Desenvolvimento de interatividade e funcionalidades dinâmicas para a web.</p>
                 </div>
             </div>
             <div className="dv-imgs">
-                <img src='../../assets/icon-react.png'/>
+                <img src={ReactImg}/>
                 <div className="texto">
                     <p> Criação de interfaces de usuário reutilizáveis e eficientes com componentes modernos.</p>
                 </div>
             </div>
             <div className="dv-imgs">
-                <img src='../../assets/icon-ts.png' />
+                <img src={Ts} />
                 <div className="texto">
                     <p>Garantia de código mais seguro e robusto com tipagem estática.</p>
                 </div>
             </div>
             <div className="dv-imgs">
-                <img src='../../assets/icon-git.png'/>
+                <img src={Git}/>
                 <div className="texto">
                     <p>Controle de versão, facilitando o gerenciamento de projetos e colaborações.</p>
                 </div>
