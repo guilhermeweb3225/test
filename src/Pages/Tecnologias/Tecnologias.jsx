3770f5c935de1bad224422533c6cc735a8f71788
Guilherme P.Oliveira <guilhermeponciano3225@gmail.com>
2024-12-29 14:36:33 -0300

    nada carai

--- src/Pages/Tecnologias/Tecnologias.jsx
+++ src/Pages/Tecnologias/Tecnologias.jsx
@@ -1,66 +1,77 @@
-import './Tecnologias.css'
-import Header from '../../componetes/header/Header'
-import Footer from '../../componetes/footer/Footer'
+import './Tecnologias.css';
+import Header from '../../componetes/header/Header';
+import Footer from '../../componetes/footer/Footer';
 
-function Tecnologias(){
-    return(
+// Importando as imagens da pasta src
+import iconHtml from '../../assets/icon-html.png';
+import iconCss from '../../assets/icon-css.png';
+import iconJsc from '../../assets/icon-Jsc.png';
+import iconReact from '../../assets/icon-react.png';
+import iconTs from '../../assets/icon-ts.png';
+import iconGit from '../../assets/icon-git.png';
+import gitIcon from '../../assets/git-icon.svg';
+import iconFigma from '../../assets/icon-figma.png';
+
+function Tecnologias() {
+  return (
     <>
-    <Header/>
-    <main>
+      <Header />
+      <main>
         <h1>Tecnologias</h1>
         <section>
-            <div className="dv-imgs">
-                <img src="../src/assets/icon-html.png" />
-                <div className="texto">
-                    <p>Criação de estrutura e conteúdo das páginas web, garantindo acessibilidade e organização.</p>
-                </div>
+          <div className="dv-imgs">
+            <img src={iconHtml} alt="HTML" />
+            <div className="texto">
+              <p>Criação de estrutura e conteúdo das páginas web, garantindo acessibilidade e organização.</p>
             </div>
-            <div className="dv-imgs">
-                <img src="../src/assets/icon-css.png" />
-                <div className="texto">
-                    <p> Estilização e layout das páginas, criando designs responsivos e visualmente atraentes.</p>
-                </div>
+          </div>
+          <div className="dv-imgs">
+            <img src={iconCss} alt="CSS" />
+            <div className="texto">
+              <p> Estilização e layout das páginas, criando designs responsivos e visualmente atraentes.</p>
             </div>
-            <div className="dv-imgs">
-                <img src='../src/assets/icon-Jsc.png' />
-                <div className="texto">
-                    <p>Desenvolvimento de interatividade e funcionalidades dinâmicas para a web.</p>
-                </div>
+          </div>
+          <div className="dv-imgs">
+            <img src={iconJsc} alt="JavaScript" />
+            <div className="texto">
+              <p>Desenvolvimento de interatividade e funcionalidades dinâmicas para a web.</p>
             </div>
-            <div className="dv-imgs">
-                <img src='../src/assets/icon-react.png'/>
-                <div className="texto">
-                    <p> Criação de interfaces de usuário reutilizáveis e eficientes com componentes modernos.</p>
-                </div>
+          </div>
+          <div className="dv-imgs">
+            <img src={iconReact} alt="React" />
+            <div className="texto">
+              <p> Criação de interfaces de usuário reutilizáveis e eficientes com componentes modernos.</p>
             </div>
-            <div className="dv-imgs">
-                <img src='../src/assets/icon-ts.png' />
-                <div className="texto">
-                    <p>Garantia de código mais seguro e robusto com tipagem estática.</p>
-                </div>
+          </div>
+          <div className="dv-imgs">
+            <img src={iconTs} alt="TypeScript" />
+            <div className="texto">
+              <p>Garantia de código mais seguro e robusto com tipagem estática.</p>
             </div>
-            <div className="dv-imgs">
-                <img src='../src/assets/icon-git.png'/>
-                <div className="texto">
-                    <p>Controle de versão, facilitando o gerenciamento de projetos e colaborações.</p>
-                </div>
+          </div>
+          <div className="dv-imgs">
+            <img src={iconGit} alt="Git" />
+            <div className="texto">
+              <p>Controle de versão, facilitando o gerenciamento de projetos e colaborações.</p>
             </div>
-            <div className="dv-imgs">
-                <img src='../src/assets/git-icon.svg'/>
-                <div className="texto">
-                    <p>Hospedagem de repositórios e trabalho em equipe com versionamento de código.</p>
-                </div>
+          </div>
+          <div className="dv-imgs">
+            <img src={gitIcon} alt="Git Icon" />
+            <div className="texto">
+              <p>Hospedagem de repositórios e trabalho em equipe com versionamento de código.</p>
             </div>
-            <div className="dv-imgs">
-                <img src='../src/assets/icon-figma.png' />
-                <div className="texto">
-                    <p> Criação de protótipos e design de interfaces, auxiliando na visualização de projetos.</p>
-                </div>
+          </div>
+          <div className="dv-imgs">
+            <img src={iconFigma} alt="Figma" />
+            <div className="texto">
+              <p> Criação de protótipos e design de interfaces, auxiliando na visualização de projetos.</p>
             </div>
+          </div>
         </section>
-    </main>
-    <Footer/>
+      </main>
+      <Footer />
     </>
-    )
+  );
 }
-export default Tecnologias
+
+export default Tecnologias;
