1ba6c751ca9d76a2317e78d71d9fafebc1dc4086
Guilherme P.Oliveira <guilhermeponciano3225@gmail.com>
2024-12-31 14:31:15 -0300

    adicionando projetos

--- src/Pages/Projetos/Projetos.jsx
+++ src/Pages/Projetos/Projetos.jsx
@@ -4,6 +4,7 @@ import Footer from '../../componetes/footer/Footer'
 import projectImg from '../../assets/projeto-img.svg'
 import imgTC from '../../assets/img-tc.png'
 import imgBoard from '../../assets/board-img.png'
+import imgBlog from '../../assets/img-blog.jpeg'
 function Projetos(){
     return (
     <>
@@ -19,48 +20,48 @@ function Projetos(){
                         <img src={imgTC} />
                     </div>
                     <h2>+TC</h2>
-                    <p className="decricao">Esta landing page apresenta uma empresa de serviços digitais focada em transformar ideias em produtos de alta qualidade. Com uma equipe experiente, oferece Gestão de Projetos, Análise de Negócios e Testes de Software para garantir eficiência, redução de erros e economia de tempo e custos.</p>
-                    <a href="https://guilhermeweb3225.github.io/TC/"><button className="BT-Primario">Abrir</button></a>
+                    <p className="descriçao">Esta landing page apresenta uma empresa de serviços digitais focada em transformar ideias em produtos de alta qualidade. Com uma equipe experiente, oferece Gestão de Projetos, Análise de Negócios e Testes de Software para garantir eficiência, redução de erros e economia de tempo e custos.</p>
+                    <a href="https://guilhermeweb3225.github.io/TC/"target='_blanck'><button className="BT-Primario">Abrir</button></a>
                 </div>
                 <div className="dv-primaria">
                     <div className="img">
                         <img src={imgBoard} />
                     </div>
                     <h2>Board de Tarefas</h2>
-                    <p className="decricao">Aplicação web simples para gerenciar usuários e suas tarefas. Permite criar,excluir e marcar tarefas como concluídas. Ideal para praticar HTML, CSS e JavaScript com foco em manipulação de dados e DOM. </p>
-                    <a href="https://guilhermeweb3225.github.io/RID-190400_Desafio3/"><button className="BT-Primario">Abrir</button></a>
+                    <p className="descriçao">Aplicação web simples para gerenciar usuários e suas tarefas. Permite criar,excluir e marcar tarefas como concluídas. Ideal para praticar HTML, CSS e JavaScript com foco em manipulação de dados e DOM. </p>
+                    <a href="https://guilhermeweb3225.github.io/RID-190400_Desafio3/"target='_blanck'><button className="BT-Primario">Abrir</button></a>
+                </div>
+                <div className="dv-primaria">
+                    <div className="img">
+                        <img src={imgBlog} />
+                    </div>
+                    <h2>Blog</h2>
+                    <p className="descriçao">Um blog totalmente responsivo,mantendo a integridade visual do site.</p>
+                    <a href="https://guilhermeweb3225.github.io/RID--190400-Desafio-2/index.html"target='_blanck'><button className="BT-Primario">clique aqui</button></a>
                 </div>
                 <div className="dv-primaria">
                     <div className="img">
                         <img src={projectImg} />
                     </div>
                     <h2>projeto</h2>
-                    <p className="decricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsa quas eveniet molestiae architecto temporibus natus. Ratione eum debitis possimus quidem quod natus dignissimos nisi sunt fugiat consequatur, quia rem?</p>
-                    <a href="#"><button className="BT-Primario">clique aqui</button></a>
+                    <p className="descriçao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsa quas eveniet molestiae architecto temporibus natus. Ratione eum debitis possimus quidem quod natus dignissimos nisi sunt fugiat consequatur, quia rem?</p>
+                    <a href="#"target='_blanck'><button className="BT-Primario">clique aqui</button></a>
                 </div>
                 <div className="dv-primaria">
                     <div className="img">
                         <img src={projectImg} />
                     </div>
                     <h2>projeto</h2>
-                    <p className="decricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsa quas eveniet molestiae architecto temporibus natus. Ratione eum debitis possimus quidem quod natus dignissimos nisi sunt fugiat consequatur, quia rem?</p>
-                    <a href="#"><button className="BT-Primario">clique aqui</button></a>
+                    <p className="descriçao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsa quas eveniet molestiae architecto temporibus natus. Ratione eum debitis possimus quidem quod natus dignissimos nisi sunt fugiat consequatur, quia rem?</p>
+                    <a href="#"target='_blanck'><button className="BT-Primario">clique aqui</button></a>
                 </div>
                 <div className="dv-primaria">
                     <div className="img">
                         <img src={projectImg} />
                     </div>
                     <h2>projeto</h2>
-                    <p className="decricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsa quas eveniet molestiae architecto temporibus natus. Ratione eum debitis possimus quidem quod natus dignissimos nisi sunt fugiat consequatur, quia rem?</p>
-                    <a href="#"><button className="BT-Primario">clique aqui</button></a>
-                </div>
-                <div className="dv-primaria">
-                    <div className="img">
-                        <img src={projectImg} />
-                    </div>
-                    <h2>projeto</h2>
-                    <p className="decricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsa quas eveniet molestiae architecto temporibus natus. Ratione eum debitis possimus quidem quod natus dignissimos nisi sunt fugiat consequatur, quia rem?</p>
-                    <a href="#"><button className="BT-Primario">clique aqui</button></a>
+                    <p className="descriçao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsa quas eveniet molestiae architecto temporibus natus. Ratione eum debitis possimus quidem quod natus dignissimos nisi sunt fugiat consequatur, quia rem?</p>
+                    <a href="#"target='_blanck'><button className="BT-Primario">clique aqui</button></a>
                 </div>
             </div>
         </main>
